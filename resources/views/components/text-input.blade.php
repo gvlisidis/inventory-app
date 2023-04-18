3a5ef5081e7c7871fdda4b005dbc8e99bb6cd975
George Vlisidis <george@mmcreative.co.uk>
2023-04-18 18:44:33 +0100

    Updated the style for the login page

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'text-gray-800 border-2 border-violet-600 focus:border-violet-600 focus:ring-violet-600 rounded-md shadow-sm']) !!}>
