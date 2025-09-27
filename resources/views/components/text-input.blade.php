874f3a19dcb4c403bcc4fb05261ce7a13caa3095
George Vlisidis <gblisidis@gmail.com>
2025-09-27 21:35:23 +0100

    Fixed login layout.

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'text-gray-800 border-2 border-curious-blue-600 focus:border-curious-blue-600 focus:ring-curious-blue-600 rounded-md shadow-xs']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'text-gray-200 border-2 border-curious-blue-600 focus:border-curious-blue-600 focus:ring-curious-blue-600 rounded-md shadow-xs']) !!}>
