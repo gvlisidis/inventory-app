b5fbdbf32f366801a4df3f203a94128368d6e7f8
George Vlisidis <gblisidis@gmail.com>
2025-09-27 12:49:23 +0100

    Comment out theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,45 +10,45 @@ module.exports = {
         "./resources/**/*.vue",
     ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                'poppins': ['Poppins', 'sans-serif']
-            },
-            colors:{
-                'curious-blue': {
-                    '50': '#eff9fc',
-                    '100': '#d6eff7',
-                    '200': '#b2dfef',
-                    '300': '#7dc7e3',
-                    '400': '#41a6cf',
-                    '500': '#2894c3',
-                    '600': '#226e98',
-                    '700': '#225b7c',
-                    '800': '#234b67',
-                    '900': '#224157',
-                    '950': '#11283b',
-                },
-            },
-            spacing:{
-                68: '17rem',
-                'left-column': '62%',
-                'right-column': '37%',
-            },
-            height: {
-                '30': '7.5rem',
-            },
-            fontSize: {
-                xxs: ['0.625rem', {lineHeight: '1rem'}],
-                label: ['1.75rem', {lineHeight: '2,15rem'}],
-            },
-            padding: {
-                '1/3': '33.33333%',
-                '2/3': '66.66667%',
-            },
+    // theme: {
+    //     extend: {
+    //         fontFamily: {
+    //             'poppins': ['Poppins', 'sans-serif']
+    //         },
+    //         colors:{
+    //             'curious-blue': {
+    //                 '50': '#eff9fc',
+    //                 '100': '#d6eff7',
+    //                 '200': '#b2dfef',
+    //                 '300': '#7dc7e3',
+    //                 '400': '#41a6cf',
+    //                 '500': '#2894c3',
+    //                 '600': '#226e98',
+    //                 '700': '#225b7c',
+    //                 '800': '#234b67',
+    //                 '900': '#224157',
+    //                 '950': '#11283b',
+    //             },
+    //         },
+    //         spacing:{
+    //             68: '17rem',
+    //             'left-column': '62%',
+    //             'right-column': '37%',
+    //         },
+    //         height: {
+    //             '30': '7.5rem',
+    //         },
+    //         fontSize: {
+    //             xxs: ['0.625rem', {lineHeight: '1rem'}],
+    //             label: ['1.75rem', {lineHeight: '2,15rem'}],
+    //         },
+    //         padding: {
+    //             '1/3': '33.33333%',
+    //             '2/3': '66.66667%',
+    //         },
 
-        },
-    },
+    //     },
+    // },
 
     plugins: [
         require('@tailwindcss/forms'),
